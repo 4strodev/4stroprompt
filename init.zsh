abd4f94a8c1cea621ecfdf41a5b886c90e3adad9
Alejandro Marin <63979138+4strodev@users.noreply.github.com>
2024-06-12 16:03:35 +0200

    Update init.zsh

--- init.zsh
+++ init.zsh
@@ -1,8 +1,19 @@
-#
-# Custom aliases/settings
-#
+setopt nopromptbang prompt{cr,percent,sp,subst}
 
-# any custom stuff should go here.
-# ensure that 'custom' exists in the zmodules array in your .zimrc
+zstyle ':zim:prompt-pwd' git-root yes
 
+typeset -gA git_info
+if (( ${+functions[git-info]} )); then
+  zstyle ':zim:git-info:branch' format '%b'
+  zstyle ':zim:git-info:commit' format '%c'
+  zstyle ':zim:git-info:clean' format '%F{green}%{%G✓%}'
+  zstyle ':zim:git-info:dirty' format '%F{yellow}%{%G✗%}'
+  zstyle ':zim:git-info:keys' format \
+      'prompt' ' %F{cyan}%b%c %C%D'
+
+  autoload -Uz add-zsh-hook && add-zsh-hook precmd git-info
+fi
+
+PS1='%b%F{white}{ %B%(?:%F{green}:%F{red})%{%G%} %b%F{white}} %F{white}$(prompt-pwd)${(e)git_info[prompt]}%f%b '
+unset RPS1
 
