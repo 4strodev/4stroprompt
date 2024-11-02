48a4be0f91c2b2e3621fc97ef34cf6120af8fac5
4strodev <jumarurex@gmail.com>
2024-11-02 13:16:40 +0100

    chore:add space to the beggining

--- init.zsh
+++ init.zsh
@@ -1,5 +1,5 @@
 init_deps
 
-PS1='%F{white}$(prompt-pwd) %b%F{white}{ %B%(?:%F{green}:%F{red})%{%G%} %b%F{white}}${(e)git_info[prompt]}%f%b '
+PS1=' %F{white}$(prompt-pwd) %b%F{white}{ %B%(?:%F{green}:%F{red})%{%G%} %b%F{white}}${(e)git_info[prompt]}%f%b '
 unset RPS1
 
