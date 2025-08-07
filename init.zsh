e2327e6498c0840131157923bbf6fdbff9ed62d8
Alejandro Marin <63979138+4strodev@users.noreply.github.com>
2025-08-07 17:25:24 +0200

    Update init.zsh

--- init.zsh
+++ init.zsh
@@ -1,5 +1,5 @@
 init_deps
 
-PS1=' %F{white}$(prompt-pwd) %b%F{white}{ %B%(?:%F{green}:%F{red})%{%G%} %b%F{white}}${(e)git_info[prompt]}%f%b\n➤ '
+PS1=$' %F{white}$(prompt-pwd) %b%F{white}{ %B%(?:%F{green}:%F{red})%{%G%} %b%F{white}}${(e)git_info[prompt]}%f%b\n ➤ '
 unset RPS1
 
